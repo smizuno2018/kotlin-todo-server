5986b3542f5753805f4725ba25d171ef1c5412f9
shoutamizuno <31718359+shoutamizuno@users.noreply.github.com>
2020-11-07 12:00:24 +0900

    make project

---
+++ src/Application.kt
@@ -0,0 +1,18 @@
+package com.todo
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    val client = HttpClient(Apache) {
+    }
+
+}
+
